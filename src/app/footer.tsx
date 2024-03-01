96a462588a93ca23b359813a8d95ac03d01300eb
Maksim Filippov <mdfilippov@edu.hse.ru>
2024-03-01 05:07:34 +0300

    pic + correct links

--- src/app/footer.tsx
+++ src/app/footer.tsx
@@ -8,15 +8,15 @@ const Footer = () => {
       <MaxWidthWrapper className="flex justify-center py-8">
         <div className="flex space-x-6">
           {/* LinkedIn Link */}
-          <Link href="https://www.linkedin.com/in/your-linkedin-profile" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-blue-600">
+          <Link href="https://www.linkedin.com/in/sollutin" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-blue-600">
             <Linkedin className="h-6 w-6" />
             <span className="sr-only">LinkedIn</span>
           </Link>
 
           {/* Twitter Link */}
-          <Link href="https://twitter.com/your-twitter-handle" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-blue-400">
+          <Link href="https://twitter.com/sollutin" target="_blank" rel="noopener noreferrer" className="text-gray-700 hover:text-blue-400">
             <X className="h-6 w-6" />
-            <span className="sr-only">Twitter</span>
+            <span className="sr-only">X</span>
           </Link>
         </div>
       </MaxWidthWrapper>
